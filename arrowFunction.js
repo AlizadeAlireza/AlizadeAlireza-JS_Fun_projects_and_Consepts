5fa68bb6d6e00e33aac96446fe2bfe25d783c3a0
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-06 14:28:58 +0330

    make random number to arrow function

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,21 +1,23 @@
 // function already create a variable of the sum in this case
 function sum(a, b) {
-  return a + b;
+    return a + b
 }
 
-let sum2 = (a, b) => a + b; // no brackets and no return statements so retrun
+let sum2 = (a, b) => a + b // no brackets and no return statements so retrun
 
 function isPositive(number) {
-  return number >= 0;
+    return number >= 0
 }
 
 // when we have a single parameter we can remove the parentheses
-let isPositive2 = (number) => number >= 0;
+let isPositive2 = (number) => number >= 0
 
 function randomNumber() {
-  return Math.random;
+    return Math.random
 }
 
+let randomNumber = () => Math.random
+
 document.addEventListener("click", function () {
-  console.log("click");
-});
+    console.log("click")
+})

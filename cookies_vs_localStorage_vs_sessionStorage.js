42d75711f6277d4d066c626dc854570d6e825296
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-13 12:05:35 +0330

    localstorage.setItem() finished

--- cookies_vs_localStorage_vs_sessionStorage.js
+++ cookies_vs_localStorage_vs_sessionStorage.js
@@ -1,4 +1,6 @@
 // first item --> key, second item --> value
 localStorage.setItem("name", "alireza")
 localStorage.setItem("family", "alizade")
-console.log(localStorage.getItem("name"))
+// remove item
+localStorage.removeItem("name")
+console.log(localStorage.getItem("name")) // will be null

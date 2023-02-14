c8585fa717663f24bf04dc665be5c242f2d57e5a
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-14 05:54:04 +0330

    session finished

--- cookies_vs_localStorage_vs_sessionStorage.js
+++ cookies_vs_localStorage_vs_sessionStorage.js
@@ -8,4 +8,6 @@ console.log(localStorage.getItem("name")) // will be null
 
 // session storage
 sessionStorage.setItem("name", "alireza")
+// for update we use the same key with the different value
+session.sessionStorage.setItem("name", "mohammad")
 console.log(sessionStorage.getItem("name"))

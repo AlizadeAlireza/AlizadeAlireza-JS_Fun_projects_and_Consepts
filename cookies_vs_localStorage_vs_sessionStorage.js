b79dec8e2565a88e881804ade185c4c290fd8391
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-14 06:04:59 +0330

    cookies finished

--- cookies_vs_localStorage_vs_sessionStorage.js
+++ cookies_vs_localStorage_vs_sessionStorage.js
@@ -9,5 +9,13 @@ console.log(localStorage.getItem("name")) // will be null
 // session storage
 sessionStorage.setItem("name", "alireza")
 // for update we use the same key with the different value
-session.sessionStorage.setItem("name", "mohammad")
+sessionStorage.setItem("name", "mohammad")
 console.log(sessionStorage.getItem("name"))
+
+// cookie storage
+// for other section of our cookie we use ;
+document.cookie = "name=alirza; expires=" + new DATE(2020, 0, 1).toUTCString()
+document.cookie =
+    "last_name=alizade; expires=" + new DATE(2020, 0, 1).toUTCString()
+
+console.log(document.cookie)

08ee49bcb9ca242887667df5380c2144004e77f4
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-22 10:27:59 +0330

    change the import name

---  ES6 modules1.js
+++  ES6 modules1.js
@@ -1,4 +1,5 @@
 import { printAge, printName } from "./ES6 modules"
+// import { printAge as printUserAge, printName } from "./ES6 modules" --> we can change the import name
 import User from "./ES6 modules"
 
 const user = new User("alireza", 22)

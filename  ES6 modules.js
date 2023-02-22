f1538055881ecf473ec2609ffb69d3a4551a2896
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-22 10:21:00 +0330

    ways of export

---
+++  ES6 modules.js
@@ -0,0 +1,5 @@
+import { printAge, printName } from "./main ES6 modules"
+import User from "./main ES6 modules"
+
+const user = new User("alireza", 22)
+console.log(user)
--- main ES6 modules.js
+++ main ES6 modules.js
@@ -3,18 +3,18 @@ the main thing about modules is to import and export different sections of code
 from different files into other files.
 */
 
-class User {
+export default class User {
     constructor(name, age) {
         this.name = name
         this.age = age
     }
 }
 
-function printName(user) {
+export function printName(user) {
     console.log(user.name)
 }
 
-function printAge(user) {
+export function printAge(user) {
     console.log(user.age)
 }
 

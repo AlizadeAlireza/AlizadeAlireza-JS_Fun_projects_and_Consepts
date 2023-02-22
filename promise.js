7fc676e7154f8ed348f4bd2df809604c5833380d
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-22 08:39:13 +0330

    promise race and all

--- promise.js
+++ promise.js
@@ -64,10 +64,53 @@ function watchTutorialPromise() {
     })
 }
 
-watchTutorialPromise().then(
-    ((message) => {
+watchTutorialPromise()
+    .then((message) => {
         console.log("Success: " + message)
-    }).catch((error) => {
+    })
+    .then((message) => {
+        console.log("Success: " + message)
+    })
+    .catch((error) => {
         console.log(error.name + " " + error.message)
     })
+
+console.log("example three \n\n\n")
+// another example of promises
+
+// all this run parallel at the same time
+
+/*
+promise.all is going to run every single one of these promises 
+and as soon as it's done it is then going to call the .then() and .catch()
+*/
+
+const recordVideoOne = new Promise((resolve, reject) => {
+    resolve("Video 1 Recorded")
+})
+
+const recordVideoTwo = new Promise((resolve, reject) => {
+    resolve("Video 2 Recorded")
+})
+
+const recordVideoThree = new Promise((resolve, reject) => {
+    resolve("Video 3 Recorded")
+})
+
+Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+    (messages) => {
+        console.log(messages)
+    }
+)
+
+/* 
+promise.race is like promise.all except it'll return as soon as the first one is completed
+instead of waiting for everything to complete.
+so it'll return just one message
+*/
+console.log("promise.race \n\n\n\n")
+Promise.race([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+    (messages) => {
+        console.log(messages)
+    }
 )

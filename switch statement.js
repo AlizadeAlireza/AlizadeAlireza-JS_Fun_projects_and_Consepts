00dca38d844bb97a53962fd7b3fe363160b02e93
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-23 16:00:54 +0330

    adding a cat case wiht same output

--- switch statement.js
+++ switch statement.js
@@ -21,6 +21,8 @@ switch (favoriteAnimal) {
     case "cat":
         console.log("cats are great")
         break
+    case "bobcat":
+        console.log("cats are great")
     case "dog":
         console.log("dogs are so smart!")
         break

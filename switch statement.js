31d5a9be74d4974ed2b1c2d0e84a33a02538a4a6
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-23 15:50:00 +0330

    the switch statement is started

---
+++ switch statement.js
@@ -0,0 +1,10 @@
+const favoriteAnimal = "cat"
+if (favoriteAnimal === "cat") {
+    console.log("Cats are great")
+} else if (favoriteAnimal === "dog") {
+    console.log("Dogs are kinda loud")
+} else if (favoriteAnimal === "shark") {
+    console.log("That's an interesting choice")
+} else {
+    console.log("I have never heard of that animal")
+}

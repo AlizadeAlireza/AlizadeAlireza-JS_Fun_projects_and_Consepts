0d9c71ea75412cbe68677dc4943029ce44cd8ff0
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-23 15:57:42 +0330

    switch with runnig to end

--- switch statement.js
+++ switch statement.js
@@ -1,10 +1,27 @@
 const favoriteAnimal = "cat"
-if (favoriteAnimal === "cat") {
-    console.log("Cats are great")
-} else if (favoriteAnimal === "dog") {
-    console.log("Dogs are kinda loud")
-} else if (favoriteAnimal === "shark") {
-    console.log("That's an interesting choice")
-} else {
-    console.log("I have never heard of that animal")
+
+// if (favoriteAnimal === "cat") {
+//     console.log("Cats are great")
+// } else if (favoriteAnimal === "dog") {
+//     console.log("Dogs are kinda loud")
+// } else if (favoriteAnimal === "shark") {
+//     console.log("That's an interesting choice")
+// } else {
+//     console.log("I have never heard of that animal")
+// }
+
+/*
+if a statement is true, the code will be run for another
+statements.
+for this situation if we want to finish the executation,
+we can use <break>
+*/
+
+switch (favoriteAnimal) {
+    case "cat":
+        console.log("cats are great")
+    case "dog":
+        console.log("dogs are so smart!")
+    case "shark":
+        console.log("sharks are dangerous")
 }

06f83cf9a8a2ffac593b4e70d3cf4e55e110d9be
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-26 19:23:26 +0330

    refrence_vs_value 1

--- refrence_vs_value.js
+++ refrence_vs_value.js
@@ -4,15 +4,17 @@ let c = a
 c = c + 1 // c is updated but the a value is like the past
 let d = [1, 2]
 let e = d
-
+let h = [4, 5, 6]
 /*
  when we add a value to the e list the d array 
  because they're address is like each other.
 */
 e.push(3)
+h.push(7)
 
 console.log(`a = ${a}`)
 console.log(`b = ${b}`)
 console.log(`c = ${c}`)
 console.log(`d = ${d}`)
-console.log(`e = ${d}`)
+console.log(`e = ${e}`)
+console.log(`h = ${h}`)

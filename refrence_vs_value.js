49b85a0eee548ff3f2c36cddc56a1ab574c11009
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-26 19:27:47 +0330

    refrence_vs_value === and == with memory addresses

--- refrence_vs_value.js
+++ refrence_vs_value.js
@@ -12,6 +12,18 @@ let h = [4, 5, 6]
 e.push(3)
 h.push(7)
 
+// they are true because their have same address
+let m = [1, 2]
+let q = m
+console.log(`m === q ${m === q}`)
+console.log(`m == q ${m == q}`)
+
+// they are false because their haven't same address
+let n = [1, 2]
+let g = [1, 2]
+console.log(`n === g ${n === g}`)
+console.log(`n == g ${n == g}`)
+
 console.log(`a = ${a}`)
 console.log(`b = ${b}`)
 console.log(`c = ${c}`)

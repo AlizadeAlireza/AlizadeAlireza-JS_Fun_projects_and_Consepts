a90a23932689ffc9540b44ab8d6031ae5fcb097d
alireza alizadeh <alireza.aa9700@gmail.com>
2023-03-10 09:40:10 +0330

    if always use ternary it's can be complicated

--- ternaryOperators.js
+++ ternaryOperators.js
@@ -11,3 +11,14 @@ let result
 result = number >= 50 ? "Not to fifiy" : "It could be worse"
 
 console.log(result)
+
+/**
+ * use this way because it's more explicit
+ */
+const user = {}
+
+if (userLeft.valid) {
+    user.save()
+} else {
+    user.printErrors()
+}

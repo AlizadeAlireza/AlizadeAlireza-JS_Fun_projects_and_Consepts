e3a340e84111c8732c726ab0b37f4bbe0c875f55
alireza alizadeh <alireza.aa9700@gmail.com>
2023-03-10 08:18:38 +0330

    use ternary variable

--- ternaryOperators.js
+++ ternaryOperators.js
@@ -1,8 +1,13 @@
 const number = 50
 let result
-if (number >= 50) {
-    result = "Not to fifty!"
-} else {
-    result = "It could be worse"
-}
+// if (number >= 50) {
+//     result = "Not to fifty!"
+// } else {
+//     result = "It could be worse"
+// }
+
+// another way
+
+result = number >= 50 ? "Not to fifiy" : "It could be worse"
+
 console.log(result)

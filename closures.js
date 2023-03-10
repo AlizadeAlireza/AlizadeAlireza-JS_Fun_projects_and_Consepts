c09df35c3263be2eda3d7b17dc0e6c64302494db
alireza alizadeh <alireza.aa9700@gmail.com>
2023-03-10 06:59:15 +0330

    the consept of closures

---
+++ closures.js
@@ -0,0 +1,20 @@
+let myName = "alireza"
+
+function printName() {
+    console.log(myName)
+}
+
+myName = "ali"
+
+printName()
+
+myName = "hasan"
+
+printName()
+
+/**
+ * we can call a variable outside of a function inside of that function
+ * that we call it cloture.
+ *
+ * whatever the most recent value of that variable is.
+ */

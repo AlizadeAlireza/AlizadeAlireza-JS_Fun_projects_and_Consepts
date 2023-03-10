b60169e7c82ef3025452f64b3ec89928963933b5
alireza alizadeh <alireza.aa9700@gmail.com>
2023-03-10 09:45:52 +0330

    another example of ternary

--- ternaryOperators.js
+++ ternaryOperators.js
@@ -1,5 +1,6 @@
 const number = 50
 let result
+let result1
 // if (number >= 50) {
 //     result = "Not to fifty!"
 // } else {
@@ -22,3 +23,22 @@ if (userLeft.valid) {
 } else {
     user.printErrors()
 }
+
+// another example
+if (number === 0) {
+    result = "You have nothing"
+} else if (number < 10) {
+    result = "You have very little"
+} else {
+    result = "You have a lot"
+}
+
+// into ternary
+// but this ternary code is also bad for understanding
+result1 =
+    number === 0
+        ? "You have nothing"
+        : number < 10
+        ? "You have very little"
+        : "You have a lot"
+console.log(result1)
